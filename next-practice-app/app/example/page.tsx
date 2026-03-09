476d58c3ce5f3fa02a7a171b249a6c91b3c14832
Naoya Yasumoto <naoyayasumoto@NaoyanoMacBook-Air.local>
2026-03-09 14:58:16 +0900

    feat(): 初期コミット確認

---
+++ next-practice-app/app/example/page.tsx
@@ -0,0 +1,10 @@
+const Example = () => {
+    return (
+            <>
+                <h1>タスク管理アプリ</h1>
+                <div>タスクを管理します</div>
+            </>
+    );
+}
+
+export default Example;
