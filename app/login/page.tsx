a397ef0bbf75e80401458d4a51c4dd772e3cc0b5
Naoya Yasumoto <naoyayasumoto@NaoyanoMacBook-Air.local>
2026-03-11 16:56:06 +0900

    fix(): ログイン画面の修正

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,31 +1,45 @@
 "use client";
 
 import { supabase } from "@/lib/supabase";
+import { Button, TextField } from "@mui/material";
+import Link from "next/link";
 import { useState } from "react";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const handleLogin = async () => {
-    await supabase.auth.signInWithOtp({ email });
-
-    alert("メールを確認してください！");
+    supabase.auth.signInWithPassword({ email, password });
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-2xl font-bold">ログイン</h1>
-      <input
-        className="border p-2 rounded"
+      <TextField
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        placeholder="メールアドレス"
+        placeholder="example@example.com"
       />
-      <button
+      <TextField
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="パスワードを入力"
+        sx={{ backgroundColor: "white" }}
+      />
+      <Button
+        variant="contained"
         className="bg-blue-500 text-white px-4 py-2 rounded"
         onClick={handleLogin}
       >
         ログイン
-      </button>
+      </Button>
+      <div>
+        <span>アカウントをお持ちでない方は</span>
+        <Link href="/signup" className="text-[var(--color-indigo-600)]">
+          サインイン
+        </Link>
+      </div>
     </div>
   );
 };
