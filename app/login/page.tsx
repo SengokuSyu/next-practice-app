8cd89c79ba62de5a42769de4e5e185d0b180d15e
Naoya Yasumoto <naoyayasumoto@NaoyanoMacBook-Air.local>
2026-03-13 11:06:21 +0900

    feat(): ログイン画面のUI作成

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,14 @@
 import { supabase } from "@/lib/supabase";
 import { LoginInput, loginSchema } from "@/schemas/login.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, TextField } from "@mui/material";
+import { MailOutline } from "@mui/icons-material";
+import {
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -35,34 +42,83 @@ const LoginPage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <p>タスク管理アプリ</p>
-      <p>メールアドレスとパスワードを入力してログインしてください</p>
-      <div>
-        <TextField {...register("email")} placeholder="example@example.com" />
-        {errors.email && <p>{errors.email.message}</p>}
-      </div>
-      <div>
-        <TextField
-          type="password"
-          {...register("password")}
-          placeholder="パスワードを入力"
-        />
-        {errors.password && <p>{errors.password.message}</p>}
-      </div>
-      <div>
-        {isLoginError && <p>メールアドレスかパスワードが間違っています</p>}
-      </div>
-      <Button type="submit" variant="contained">
-        ログイン
-      </Button>
-      <div>
-        <span>アカウントをお持ちでない方は</span>
-        <Link href="/signup" className="text-[var(--color-indigo-600)]">
-          サインイン
-        </Link>
-      </div>
-    </form>
+    <div className="min-h-screen flex items-center justify-center bg-slate-200">
+      <Card className="w-[480px] shadow-xl !rounded-[12px]">
+        <CardContent className="p-10 gap-6 flex flex-col justify-center">
+          <div className="flex flex-col gap-1">
+            {/* icon */}
+            <div className="flex justify-center mb-4">
+              <div className="bg-indigo-500 text-white p-3 rounded-full">
+                <MailOutline />
+              </div>
+            </div>
+
+            {/* title */}
+            <Typography variant="h5" align="center" className="font-bold !mb-2">
+              タスク管理アプリ
+            </Typography>
+            <Typography align="center" className="text-gray-500 mb-6">
+              メールアドレスとパスワードを入力してログインしてください
+            </Typography>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div className="mb-4">
+              <Typography className="mb-2 text-sm font-medium">
+                メールアドレス
+              </Typography>
+              <TextField
+                fullWidth
+                {...register("email")}
+                placeholder="example@example.com"
+              />
+              {errors.email && (
+                <Typography color="error">{errors.email.message}</Typography>
+              )}
+            </div>
+
+            <div className="mb-4">
+              <Typography className="mb-2 text-sm font-medium">
+                パスワード
+              </Typography>
+              <TextField
+                fullWidth
+                type="password"
+                {...register("password")}
+                placeholder="パスワードを入力"
+              />
+              {errors.password && (
+                <Typography color="error">{errors.password.message}</Typography>
+              )}
+            </div>
+
+            <div>
+              {isLoginError && (
+                <Typography color="error" className="text-sm">
+                  メールアドレスかパスワードが間違っています
+                </Typography>
+              )}
+            </div>
+            <Button
+              type="submit"
+              variant="contained"
+              fullWidth
+              className="!x-366 !y-20 !bg-indigo-500 !text-white !mb-4 !py-2 !px-4"
+            >
+              ログイン
+            </Button>
+            <Typography align="center" className="text-sm text-black-500 mt-6">
+              アカウントをお持ちでない方は{" "}
+              <Link
+                href="/signup"
+                className="text-[var(--color-indigo-600)] font-medium"
+              >
+                サインイン
+              </Link>
+            </Typography>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
